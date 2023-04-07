8d3513a9f60170e2aed5b92030ca2815430ea9b4
Zahid Khan <Zahid.khan@globalpay.com>
2023-04-07 22:43:50 +0530

    Change package naming

--- src/main/java/com/baeldung/warnings/DemoApplication.java
+++ src/main/java/com/baeldung/warnings/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.bealdung.warnings;
+package com.baeldung.warnings;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/baeldung/warnings/configuration/MainConfiguration.java
+++ src/main/java/com/baeldung/warnings/configuration/MainConfiguration.java
@@ -1,7 +1,6 @@
-package com.bealdung.warnings.configuration;
+package com.baeldung.warnings.configuration;
 
 import jakarta.annotation.PostConstruct;
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,11 +14,19 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ConfigurationProperties(prefix = "profile.application-properties")
-@Data
 public class MainConfiguration {
-	String name;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(final String name) {
+		this.name = name;
+	}
+	
 	@PostConstruct
-	void print(){
+	void print() {
 		System.err.println(name);
 	}
 }
